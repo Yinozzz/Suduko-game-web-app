e481569a27285922f3fb4338eab19bd4aea084ab
Yino <526319623@qq.com>
2022-05-05 17:12:39 +0800

    develop login function

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template
+from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for
 from app.forms import RegisterForm
 from app.models import User
 
@@ -13,3 +14,15 @@ def index():
 def register():
     form = RegisterForm()
     return render_template('register.html', form=form)
+
+@app.route('/login',methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+
+
