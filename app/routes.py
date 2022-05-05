29c52decae65a516fa602b6cff9b14f90b83c9df
liu <22924454@student.uwa.edu.au>
2022-05-05 16:59:11 +0800

    create a user table, and develop register.html

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,15 @@
 from app import app
+from flask import render_template
+from app.forms import RegisterForm
+from app.models import User
 
 
 @app.route('/')
 def index():
     return "Hello, world"
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    form = RegisterForm()
+    return render_template('register.html', form=form)
