60a65e5c76452c53c689f5f7169fcf5724773b95
Yino <526319623@qq.com>
2022-05-22 00:16:57 +0800

    delete unused parameter and modify the page title

--- tests/systemtest.py
+++ tests/systemtest.py
@@ -6,17 +6,10 @@ from datetime import date
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-# Set test variables for test admin user
-test_admin_username = "admin"
-test_admin_password = "123456"
 
-# Set test variables for test user1
-test_user1_username = "Tom"
-test_user1_password ="jdkfjs"
 
 # Set test variables for test game
 test_game1 = "7,6,3,1,9,8,2,5,4,4,9,5,6,7,2,8,3,1,1,2,8,3,5,4,9,6,7,2,4,9,8,6,5,1,7,3,3,5,7,9,2,1,4,8,6,8,1,6,4,3,7,5,2,9,6,8,1,2,4,3,7,9,5,5,3,2,7,1,9,6,4,8,9,7,4,5,8,6,3,1,2"
-test_game1_uploadId = '1'
 
 
 class SystemTest(unittest.TestCase):
